bc906f3bf1cac53efbd530ae0821b5322bf2ca65
unknown <sashamangir@mail.ru>
2017-09-14 13:05:16 +0300

    1

--- base.sql
+++ base.sql
@@ -1,4 +1,4 @@
-BEGIN
+BEGIN;
 CREATE TABLE user (
     id SERIAL PRIMARY KEY, 
     name VARCHAR(100),
@@ -12,9 +12,23 @@ CREATE TABLE account (
     user_id INTEGER REFERENCES user(id)
 
 );
+CREATE TABLE post(
+    id SERIAL PRIMARY KEY, 
+    title VARCHAR(200),
+    post TEXT,
+    created_at TIMESTAMP,
+    post_id INTEGER REFERENCES post(id),
+    user_id INTEGER REFERENCES user(id)
+);
+CREATE TABLE likes(
+    user_id INTEGER REFERENCES user(id),
+    post_id INTEGER REFERENCES post(id),
+    user_like BOOLEAN,
+    PRIMARY KEY (user_id, post_id)
+);
 COMMIT;
 
-SET search_path TO forum, public;
+--SET search_path TO forum, public;
 
 
 INSERT INTO forum.user VALUES 
